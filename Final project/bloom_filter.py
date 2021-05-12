036f87bf87e679ea1241c96ccbde7ba2e98b2c40
Kaloyan Kulov <kaloyankulov2003kk@gmail.com>
2021-05-12 22:28:08 +0300

    Worked on the final project

--- Final project/bloom_filter.py
+++ Final project/bloom_filter.py
@@ -2,9 +2,12 @@
 # Install mmh3 and bitarray 3rd party module first
 # pip install mmh3
 # pip install bitarray
+from random import shuffle
 import math
 import mmh3
 from bitarray import bitarray
+import time
+import sys
 
 class BloomFilter(object):
     '''
@@ -70,6 +73,51 @@ class BloomFilter(object):
         # Return the count of hash functions needed using the formula k = (m/n) * lg(2)
         k = (m / n) * math.log(2)
         return int(k)
+    
+
+n = 2 # no of items to add
+p = 0.5 # false positive probability
+
+bloomf = BloomFilter(n,p)
+print(f"Size of bit array: {bloomf.size}")
+print(f"False positive Probability: {bloomf.fp_prob} %")
+print(f"Number of hash functions: {bloomf.hash_count}\n")
+
+word_present = [
+            "2s","3s","4s","5s","6s","7s","8s","9s","10s","Js","Qs","Ks","As"
+            "2h","3h","4h","5h","6h","7h","8h","9h","10h","Jh","Qh","Kh","Ah"
+            "2d","3d","4d","5d","6d","7d","8d","9d","10d","Jd","Qd","Kd","Ad"
+            "2c","3c","4c","5c","6c","7c","8c","9c","10c","Jc","Qc","Kc","Ac"
+           ]
+
+word_absent = ['bluff','cheater','hate','war','humanity',
+			'racism','hurt','nuke','gloomy','facebook',
+			'geeksforgeeks','twitter']
+
+print("#1")
+t0 = time.perf_counter()
+for item in word_present:
+	bloomf.add(item)
+t1 = time.perf_counter()
+print(t1 - t0)
 
 
-# def test_bloom_filter()
+shuffle(word_present)
+shuffle(word_absent)
+
+test_words = word_present[:10] + word_absent
+shuffle(test_words)
+for word in test_words:
+	if bloomf.check(word):
+		if word in word_absent:
+			print(f"{word} is a false positive!")
+		else:
+			print(f"{word} is probably present!")
+	else:
+		print(f"{word} is definitely not present!")
+
+
+print("=" * 20)
+print(sys.getsizeof(word_present))
+print(sys.getsizeof(bloomf))
+
